8a74d224c81bb9b736d5b10bbbcd134708fe362e
Andreea <eduard.parvu@rinftech.com>
2022-08-16 22:51:16 +0300

    use react redux

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -4,7 +4,8 @@ import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
-import { useStoreContext } from "../../app/context/StoreContext";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
 
 interface Props {
     product: Product,
@@ -13,12 +14,12 @@ interface Props {
 export default function ProductCard({ product }: Props) {
 
     const [loading, setLoading] = useState<boolean>(false);
-    const { setBasket } = useStoreContext();
+    const dispatch = useAppDispatch();
 
     function handleAddItem(productId: string) {
         setLoading(true);
         agent.Basket.addItem(productId)
-            .then(basket => setBasket(basket))
+            .then(basket => dispatch(setBasket(basket)))
             .catch(error => console.log(error))
             .finally(() => setLoading(false))
     }
