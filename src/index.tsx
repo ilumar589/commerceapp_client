39ae6c86b18be0e3e66a08198b22e296af6207dc
Andreea <eduard.parvu@rinftech.com>
2022-08-23 13:06:09 +0300

    some more login features

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,25 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import App from './app/layout/App';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore';
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
+const history = createBrowserHistory({ window });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+export default history;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HistoryRouter history={history}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HistoryRouter>
   </React.StrictMode>
 );
